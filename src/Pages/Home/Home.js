cfb7d4c0b7f97fd8f46047df1c3009c71ee6579a
unknown <latapata1952@gmail.com>
2022-04-17 15:21:17 +0600

    page components setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+// #61b5e4
+// #a3daff
+// #52b2fe
+// #5ec2ed
+// #41c1ef
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Feel Better, move better, be better</h1>
+        </div>
+    );
+};
+
+export default Home;
