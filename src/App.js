cee393ca3ad4c5da0710365c098ef00a936804d4
unknown <latapata1952@gmail.com>
2022-04-17 16:06:28 +0600

    header component setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Header from './Shared/Header/Header';
+import Blogs from './Pages/Blogs/Blogs';
+import About from './Pages/About/About';
+import Register from './Pages/Register/Register';
+import SignIn from './Pages/SignIn/SignIn';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/signin' element={<SignIn></SignIn>}></Route>
+        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.PNG';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <header>
+            <Link to='/'><img src={logo} alt="" /></Link>
+            <nav>
+                <Link to='/home'>Home</Link>
+                <Link to='/blogs'>Blogs</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/register'>Register</Link>
+                <Link to='/signin'>Sign In</Link>
+            </nav>
+        </header>
     );
 };
 
