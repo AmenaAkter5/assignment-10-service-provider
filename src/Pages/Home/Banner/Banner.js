16e604d8ba6c22fdb938f2ec6c03b4af6bdd869c
unknown <latapata1952@gmail.com>
2022-04-18 22:14:56 +0600

    submit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './Banner.css';
 import banner from '../../../images/banner.jpg';
+import { useNavigate } from 'react-router-dom';
 
 const Banner = () => {
+
+    const navigate = useNavigate();
+
+    const aboutMebuttonHandle = () => {
+        navigate('/about');
+    }
+
     return (
         <section className='banner'>
             <div className='banner-container container d-flex justify-content-between align-items-center'>
                 <div>
                     <h5 className='banner-heading mb-3'>HELPING YOU LIVE LIFE</h5>
                     <h1 className='banner-title mb-4'>Feel Better, move better, be better</h1>
-                    <button className='banner-btn'>Book Appointment</button>
+                    <button onClick={aboutMebuttonHandle} className='banner-btn'>Know More Details</button>
                 </div>
                 <div>
                     <img className='banner-img img-fluid' src={banner} alt="" />
